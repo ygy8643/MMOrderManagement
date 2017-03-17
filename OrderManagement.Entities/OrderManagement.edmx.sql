3bd5ab4291d7d6a1c3c94f46359028cc05ac494a
IDH303 <IDH303@PC3854>
2017-03-17 11:44:42 +0900

    Create db sql.

--- OrderManagement.Entities/OrderManagement.edmx.sql
+++ OrderManagement.Entities/OrderManagement.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 03/10/2017 08:46:24
--- Generated from EDMX file: D:\OrderManagement\OrderManagement.Entities\OrderInfo.edmx
+-- Date Created: 03/17/2017 11:42:51
+-- Generated from EDMX file: C:\Users\idh303\Source\Repos\MMOrderManagement\OrderManagement.Entities\OrderManagement.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
@@ -18,39 +18,53 @@ GO
 -- --------------------------------------------------
 
 IF OBJECT_ID(N'[dbo].[FK_Order_Customer]', 'F') IS NOT NULL
-    ALTER TABLE [dbo].[Order] DROP CONSTRAINT [FK_Order_Customer];
+    ALTER TABLE [dbo].[Orders] DROP CONSTRAINT [FK_Order_Customer];
 GO
 IF OBJECT_ID(N'[dbo].[FK_OrderDetail_Order]', 'F') IS NOT NULL
-    ALTER TABLE [dbo].[OrderDetail] DROP CONSTRAINT [FK_OrderDetail_Order];
+    ALTER TABLE [dbo].[OrderDetails] DROP CONSTRAINT [FK_OrderDetail_Order];
 GO
 IF OBJECT_ID(N'[dbo].[FK_OrderDetail_Product]', 'F') IS NOT NULL
-    ALTER TABLE [dbo].[OrderDetail] DROP CONSTRAINT [FK_OrderDetail_Product];
+    ALTER TABLE [dbo].[OrderDetails] DROP CONSTRAINT [FK_OrderDetail_Product];
+GO
+IF OBJECT_ID(N'[dbo].[FK_Product_Brand]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Products] DROP CONSTRAINT [FK_Product_Brand];
 GO
 
 -- --------------------------------------------------
 -- Dropping existing tables
 -- --------------------------------------------------
 
-IF OBJECT_ID(N'[dbo].[Customer]', 'U') IS NOT NULL
-    DROP TABLE [dbo].[Customer];
+IF OBJECT_ID(N'[dbo].[Brands]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Brands];
 GO
-IF OBJECT_ID(N'[dbo].[Order]', 'U') IS NOT NULL
-    DROP TABLE [dbo].[Order];
+IF OBJECT_ID(N'[dbo].[Customers]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Customers];
 GO
-IF OBJECT_ID(N'[dbo].[OrderDetail]', 'U') IS NOT NULL
-    DROP TABLE [dbo].[OrderDetail];
+IF OBJECT_ID(N'[dbo].[OrderDetails]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[OrderDetails];
 GO
-IF OBJECT_ID(N'[dbo].[Product]', 'U') IS NOT NULL
-    DROP TABLE [dbo].[Product];
+IF OBJECT_ID(N'[dbo].[Orders]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Orders];
+GO
+IF OBJECT_ID(N'[dbo].[Products]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Products];
 GO
 
 -- --------------------------------------------------
 -- Creating all tables
 -- --------------------------------------------------
 
+-- Creating table 'Brands'
+CREATE TABLE [dbo].[Brands] (
+    [BrandId] int IDENTITY(1,1) NOT NULL,
+    [BrandName] nvarchar(50)  NULL,
+    [BrandNameJp] nvarchar(50)  NULL
+);
+GO
+
 -- Creating table 'Customers'
 CREATE TABLE [dbo].[Customers] (
-    [CustomerId] int  NOT NULL,
+    [CustomerId] int IDENTITY(1,1) NOT NULL,
     [Name] nvarchar(50)  NULL,
     [Address] nvarchar(max)  NULL,
     [PostCode] nchar(20)  NULL,
@@ -58,32 +72,36 @@ CREATE TABLE [dbo].[Customers] (
 );
 GO
 
--- Creating table 'Orders'
-CREATE TABLE [dbo].[Orders] (
-    [OrderId] int  NOT NULL,
-    [CustomerId] int  NOT NULL,
-    [OrderDate] datetime  NULL,
-    [ShipDate] datetime  NULL
-);
-GO
-
 -- Creating table 'OrderDetails'
 CREATE TABLE [dbo].[OrderDetails] (
-    [OrderDetailId] int  NOT NULL,
+    [OrderDetailId] int IDENTITY(1,1) NOT NULL,
     [OrderId] int  NOT NULL,
     [ProductId] int  NOT NULL,
     [Quantity] int  NULL,
     [PurchasePrice] decimal(19,4)  NULL,
     [SoldPrice] decimal(19,4)  NULL,
+    [Status] int  NOT NULL,
     [Link] nvarchar(max)  NULL
+);
+GO
+
+-- Creating table 'Orders'
+CREATE TABLE [dbo].[Orders] (
+    [OrderId] int IDENTITY(1,1) NOT NULL,
+    [CustomerId] int  NOT NULL,
+    [OrderType] int  NOT NULL,
+    [InvoiceNo] nvarchar(30)  NULL,
+    [OrderDate] datetime  NULL,
+    [ShipDate] datetime  NULL
 );
 GO
 
 -- Creating table 'Products'
 CREATE TABLE [dbo].[Products] (
-    [ProductId] int  NOT NULL,
-    [ProductNameCN] nvarchar(max)  NULL,
-    [ProductNameJP] nvarchar(max)  NULL
+    [ProductId] int IDENTITY(1,1) NOT NULL,
+    [BrandId] int  NOT NULL,
+    [ProductName] nvarchar(max)  NULL,
+    [ProductNameJp] nvarchar(max)  NULL
 );
 GO
 
@@ -91,22 +109,28 @@ GO
 -- Creating all PRIMARY KEY constraints
 -- --------------------------------------------------
 
+-- Creating primary key on [BrandId] in table 'Brands'
+ALTER TABLE [dbo].[Brands]
+ADD CONSTRAINT [PK_Brands]
+    PRIMARY KEY CLUSTERED ([BrandId] ASC);
+GO
+
 -- Creating primary key on [CustomerId] in table 'Customers'
 ALTER TABLE [dbo].[Customers]
 ADD CONSTRAINT [PK_Customers]
     PRIMARY KEY CLUSTERED ([CustomerId] ASC);
 GO
 
--- Creating primary key on [OrderId] in table 'Orders'
-ALTER TABLE [dbo].[Orders]
-ADD CONSTRAINT [PK_Orders]
-    PRIMARY KEY CLUSTERED ([OrderId] ASC);
-GO
-
 -- Creating primary key on [OrderDetailId] in table 'OrderDetails'
 ALTER TABLE [dbo].[OrderDetails]
 ADD CONSTRAINT [PK_OrderDetails]
     PRIMARY KEY CLUSTERED ([OrderDetailId] ASC);
+GO
+
+-- Creating primary key on [OrderId] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [PK_Orders]
+    PRIMARY KEY CLUSTERED ([OrderId] ASC);
 GO
 
 -- Creating primary key on [ProductId] in table 'Products'
@@ -118,6 +142,21 @@ GO
 -- --------------------------------------------------
 -- Creating all FOREIGN KEY constraints
 -- --------------------------------------------------
+
+-- Creating foreign key on [BrandId] in table 'Products'
+ALTER TABLE [dbo].[Products]
+ADD CONSTRAINT [FK_Product_Brand]
+    FOREIGN KEY ([BrandId])
+    REFERENCES [dbo].[Brands]
+        ([BrandId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Product_Brand'
+CREATE INDEX [IX_FK_Product_Brand]
+ON [dbo].[Products]
+    ([BrandId]);
+GO
 
 -- Creating foreign key on [CustomerId] in table 'Orders'
 ALTER TABLE [dbo].[Orders]
